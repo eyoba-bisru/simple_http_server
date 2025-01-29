beea0a34256657bd696c6373e05cdc6ed2ab8568
Eyob <eabebe91@gmail.com>
2025-01-29 17:49:25 +0300

    feat: writing to log files added

--- src/index.js
+++ src/index.js
@@ -1,36 +1,49 @@
 import fs from 'node:fs'
 import http from 'node:http'
 import path from 'node:path'
-import logger from './logger.js'
+import logger, { dateFormat } from './logger.js'
 
 const PORT = 8000
 
 const server = http.createServer()
 
 server.on('request', (req, res) => {
-    const url = req.url
 
-    const loggerValue = logger(url)
-    console.log(loggerValue)
-
-    if (url === '/') {
-        return res.end(fs.readFileSync(path.join(process.cwd(), 'static', 'index.html')))
-    }
-
-    const static_path = path.join(process.cwd(), 'static', url)
-
-    if (!fs.existsSync(static_path) || !fs.statSync(static_path).isFile()) {
-        res.writeHead(404, { 'Content-Type': 'text/html' })
-        return res.end(fs.readFileSync(path.join(process.cwd(), 'static', 'not-found.html')))
-    }
+    const logPath = './logs/log-' + dateFormat((new Date())).toString().split(',')[0].split('/').join('-') + '.txt'
 
     try {
-        // const readStream = fs.createReadStream(static_path)
-        // readStream.pipe(res)
-        res.writeHead(200, { 'Content-Type': 'text/html' })
-        return res.end(fs.readFileSync(static_path))
+
+        const url = req.url
+
+        const loggerValue = logger(url)
+        fs.appendFile(logPath, (loggerValue + "\n"), (err) => {
+            if (err) console.log(err)
+        })
+
+        console.log(loggerValue)
+
+        if (url === '/') {
+            return res.end(fs.readFileSync(path.join(process.cwd(), 'static', 'index.html')))
+        }
+
+        const static_path = path.join(process.cwd(), 'static', url)
+
+        if (!fs.existsSync(static_path) || !fs.statSync(static_path).isFile()) {
+            res.writeHead(404, { 'Content-Type': 'text/html' })
+            return res.end(fs.readFileSync(path.join(process.cwd(), 'static', 'not-found.html')))
+        }
+
+        try {
+            res.writeHead(200, { 'Content-Type': 'text/html' })
+            return res.end(fs.readFileSync(static_path))
+        } catch (err) {
+            console.log(err)
+        }
+
     } catch (err) {
-        console.log(err)
+        fs.appendFile(logPath, err.toString(), (err) => {
+            if (err) console.log(err)
+        })
     }
 
 })
--- src/logger.js
+++ src/logger.js
@@ -2,6 +2,6 @@ export default function logger(url) {
     return (url + " " + dateFormat(new Date()))
 }
 
-function dateFormat(date) {
+export function dateFormat(date) {
     return date.toLocaleString()
 }
